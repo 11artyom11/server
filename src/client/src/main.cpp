f1493739341cda7d02e1ec15843b037a0a304d52
Artyom <grigorianartyom1@gmail.com>
2022-09-11 19:32:14 +0400

    Fixed some moments

--- src/client/src/main.cpp
+++ src/client/src/main.cpp
@@ -2,24 +2,6 @@
 
 using namespace Client;
 
-
-void greet_message ()
-{
-  std::cout << "******************************************************************\n";
-  std::cout << "*       _________.___.____ ____   _________________________      *\n";
-  std::cout << "*      /   _____/|   |    |\\   \\ /   /\\_   _____/\\______   \\     *\n";
-  std::cout << "*      \\_____  \\ |   |    | \\   Y   /  |    __)_  |       _/     *\n";
-  std::cout << "*      /        \\|   |    |__\\     /   |        \\ |    |   \\     *\n";
-  std::cout << "*     /_______  /|___|_______ \\___/   /_______  / |____|_  /     *\n";
-  std::cout << "*             \\/             \\/               \\/         \\/      *\n";
-  std::cout << "* 1 - Config connection                                          *\n";
-  std::cout << "* 2 - Create Chatroom                                            *\n";
-  std::cout << "* 3 - Get Chatroom info                                          *\n";
-  std::cout << "* 4 - Open Chatroom                                              *\n";
-  std::cout << "******************************************************************\n";
-  return;
-}
-
 int main(int argc, char* argv[]) {
   if (argc != 2) {
     Debug().fatal("1 parameter expected (port) ", argc,
